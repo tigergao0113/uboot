752c73dc9a65af3d946474c6aec3fcb4b80206f2
tigergao0113 <gxhlct@gmail.com>
2015-08-20 14:34:12 +0800

    uboot for Infinova

---
+++ uboot2013Q2/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,2 @@
+#define U_BOOT_DATE "Aug 20 2015"
+#define U_BOOT_TIME "11:56:38"
---
+++ uboot2013Q2/include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2011.12"
+#define U_BOOT_VERSION "U-Boot 2011.12"
+#define CC_VERSION_STRING "arm-marvell-linux-gnueabi-gcc (Linaro GCC branch-4.6.4. Marvell GCC 201301-1645.aee66e26) 4.6.4 20120731 (prerelease)"
+#define LD_VERSION_STRING "GNU ld (Linaro GCC branch-4.6.4. Marvell GCC 201301-1645.aee66e26) 2.22.0.20120801"
